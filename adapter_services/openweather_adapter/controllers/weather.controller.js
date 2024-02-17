36578cef0960bbe4c91de9b56506c2f58816a0c3
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-17 11:26:35 +0100

    Added controller for forecast

--- adapter_services/openweather_adapter/controllers/weather.controller.js
+++ adapter_services/openweather_adapter/controllers/weather.controller.js
@@ -1,8 +1,136 @@
 const axios = require("axios");
+const apiKey = process.env.OPENWEATHER_API_KEY;
+const units = `metric`;
 
 const getWeather = async (req, res) => {
-    let lat = req.query.lat;
-    let lon = req.query.lon;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather`;
+
+    await axios.get(apiUrl, {
+        params: {
+            lat: lat,
+            lon: lon,
+            units: units,
+            appid: apiKey
+        }
+    }).then((response) => {
+        if (response === null || response === undefined) {
+            return res
+                .status(500)
+                .send({
+                    "status": "error",
+                    "message": "No data received from Open Weather"
+                });
+        }
+
+        let weather = {};
+        weather['condition'] = response.data.weather[0].main;
+        weather['description'] = response.data.weather[0].description;
+        weather['temp'] = response.data.main.temp;
+        weather['perceivedTemp'] = response.data.main.feels_like;
+        weather['humidity'] = response.data.main.humidity;
+        weather['visibility'] = response.data.visibility;
+        weather['wind'] = response.data.wind.speed;
+        weather['clouds'] = response.data.clouds.all;
+        weather['dt'] = response.data.dt;
+        weather['timezone'] = response.data.timezone;
+        weather['sunrise'] = response.data.sys.sunrise;
+        weather['sunset'] = response.data.sys.sunset;
+
+        return res
+            .status(200)
+            .json({
+                "status": "success",
+                "data": {
+                    "weather": weather
+                }
+            });
+
+    }).catch((error) => {
+        return res
+            .status(500)
+            .send({
+                "status": "error",
+                "message": error
+            })
+    });
+}
+
+const getForecast = async (req, res) => {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast`;
+
+    await axios.get(apiUrl, {
+        params: {
+            lat: lat,
+            lon: lon,
+            units: units,
+            appid: apiKey
+        }
+    }).then((response) => {
+        if (response === null || response === undefined) {
+            return res
+                .status(500)
+                .send({
+                    "status": "error",
+                    "message": "No data received from Open Weather"
+                });
+        }
+
+        let info = {};
+        info['timezone'] = response.data.city.timezone;
+        info['sunrise'] = response.data.city.sunrise;
+        info['sunset'] = response.data.city.sunset;
+
+        let data = response.data.list;
+
+        let forecasts = [];
+        for (let i = 0; i < data.length; i++) {
+            let forecast = {};
+
+            forecast['condition'] = data[i].weather[0].main;
+            forecast['description'] = data[i].weather[0].description;
+            forecast['temp'] = data[i].main.temp;
+            forecast['perceivedTemp'] = data[i].main.feels_like;
+            forecast['humidity'] = data[i].main.humidity;
+            forecast['visibility'] = data[i].visibility;
+            forecast['wind'] = data[i].wind.speed;
+            forecast['clouds'] = data[i].clouds.all;
+            data[i].rain ? forecast['rain'] = data[i].rain['3h'] : forecast['rain'] = 0.0;
+            data[i].snow ? forecast['snow'] = data[i].snow['3h'] : forecast['snow'] = 0.0;
+            data[i]['pop'] ? forecast['probability'] = data[i]['pop'] : forecast['probability'] = 0.0;
+            forecast['dt'] = data[i].dt;
+
+            forecasts.push(forecast);
+        }
+
+        return res
+            .status(200)
+            .json({
+                "status": "success",
+                "data": {
+                    "forecast": forecasts,
+                    "info": info
+                }
+            });
+
+    }).catch((error) => {
+        return res
+            .status(500)
+            .send({
+                "status": "error",
+                "message": error
+            })
+    });
+}
+
+const coordinateValidator = (req, res, next) => {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
 
     if (!lat) {
         return res
@@ -70,64 +198,11 @@ const getWeather = async (req, res) => {
             });
     }
 
-    let apiKey = process.env.OPENWEATHER_API_KEY;
-    let units = `metric`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather`;
-    let apiUrlComplete = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-
-    await axios.get(apiUrl, {
-        params: {
-            lat: lat,
-            lon: lon,
-            units: units,
-            appid: apiKey
-        }
-    }).then((response) => {
-        if (response === null || response === undefined) {
-            return res
-                .status(500)
-                .send({
-                    "status": "error",
-                    "message": "No data received from Open Weather"
-                });
-        }
-
-        let weather = {};
-        weather['condition'] = response.data.weather[0].main;
-        weather['description'] = response.data.weather[0].description;
-        weather['temp'] = response.data.main.temp;
-        weather['perceivedTemp'] = response.data.main.feels_like;
-        weather['humidity'] = response.data.main.humidity;
-        weather['visibility'] = response.data.visibility;
-        weather['wind'] = response.data.wind;
-        weather['clouds'] = response.data.clouds.all;
-        weather['dt'] = response.data.dt;
-        weather['timezone'] = response.data.timezone;
-        weather['sunrise'] = response.data.sys.sunrise;
-        weather['sunset'] = response.data.sys.sunset;
-
-        return res
-            .status(200)
-            .json({
-                "status": "success",
-                "data": {
-                    "weather": weather
-                }
-            });
-
-    }).catch((error) => {
-        return res
-            .status(500)
-            .send({
-                "status": "error",
-                "message": error
-            })
-    });
+    next();
 }
 
-const getForecast = (req, res) => {}
-
 module.exports = {
+    coordinateValidator,
     getWeather,
     getForecast
 }
