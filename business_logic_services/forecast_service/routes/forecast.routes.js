3a918379fff8db4914acc47c75cdedf1cd5c3854
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-18 21:54:07 +0100

    Added routes in forecast service

--- business_logic_services/forecast_service/routes/forecast.routes.js
+++ business_logic_services/forecast_service/routes/forecast.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { coordinateValidation, forecast, weather } = require("../controllers/forecast.controller");
 const router = express.Router();
 
-
+router.get('/next', coordinateValidation, forecast);
+router.get('/now', coordinateValidation, weather);
 
 module.exports = router;
