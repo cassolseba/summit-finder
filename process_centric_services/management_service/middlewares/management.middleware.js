8d31d686a48b16f8d9234afcbef92631eaa13751
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-18 16:24:57 +0100

    Added wish validation in management controller

--- process_centric_services/management_service/middlewares/management.middleware.js
+++ process_centric_services/management_service/middlewares/management.middleware.js
@@ -33,11 +33,172 @@ const verifyToken = async (req, res, next) => {
         } else {
             req.userId = payload.user.id;
             req.username = payload.user.username;
+            req.email = payload.user.email;
+            req.admin = payload.user.admin;
         }
     });
     next();
 }
 
+const wishValidation = (req, res, next) => {
+    if (!req.body.originLat) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLat": "The latitude of the origin is required"
+                }
+            });
+    }
+
+    if (isNaN(req.body.originLat)) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLat": "The latitude of the origin must be a decimal number"
+                }
+            });
+    }
+
+    if (req.body.originLat > 180 || req.body.originLat < -180) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLat": "The latitude of the origin must be a decimal number between 180 and -180"
+                }
+            });
+    }
+
+    if (!req.body.originLon) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLon": "The longitude of the origin is required"
+                }
+            });
+    }
+
+    if (isNaN(req.body.originLon)) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLon": "The longitude of the origin must be a decimal number"
+                }
+            });
+    }
+
+    if (req.body.originLon > 180 || req.body.originLon < -180) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originLon": "The longitude of the origin must be a decimal number between 180 and -180"
+                }
+            });
+    }
+
+    if (!req.body.originName) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "originName": "The name of the origin is required"
+                }
+            });
+    }
+
+    if (!req.body.destinationLat) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLat": "The latitude of the destination is required"
+                }
+            });
+    }
+
+    if (isNaN(req.body.destinationLat)) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLat": "The latitude of the destination must be a decimal number"
+                }
+            });
+    }
+
+    if (req.body.destinationLat > 180 || req.body.destinationLat < -180) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLat": "The latitude of the destination must be a decimal number between 180 and -180"
+                }
+            });
+    }
+
+    if (!req.body.destinationLon) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLon": "The longitude of the destination is required"
+                }
+            });
+    }
+
+    if (isNaN(req.body.destinationLon)) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLon": "The longitude of the destination must be a decimal number"
+                }
+            });
+    }
+
+    if (req.body.destinationLon > 180 || req.body.destinationLon < -180) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationLon": "The longitude of the destination must be a decimal number between 180 and -180"
+                }
+            });
+    }
+
+    if (!req.body.destinationName) {
+        return res
+            .status(400)
+            .send({
+                "status": "fail",
+                "data": {
+                    "destinationName": "The name of the destination is required"
+                }
+            });
+    }
+
+    next();
+}
+
 module.exports = {
-    verifyToken
+    verifyToken,
+    wishValidation
 }
