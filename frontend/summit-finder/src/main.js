494b31fa8da6464555f1d4353f84a01fb3dfc300
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-19 23:31:01 +0100

    Loaded packages on main

--- frontend/summit-finder/src/main.js
+++ frontend/summit-finder/src/main.js
@@ -1,9 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from 'vuetify'
-import 'Vuetify/dist/vuetify.min.css'
+import router from "./router"
+import store from './store';
+
+// Vuetify
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { md1 } from 'vuetify/blueprints';
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'light'
+    },
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    components,
+    directives,
+    blueprint: md1,
+})
 
 const app = createApp(App);
 app
+    .use(store)
     .use(vuetify)
+    .use(router)
     .mount('#app');
