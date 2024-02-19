8eb5af2abc11bb428bfe101ae2898d1e768ad17d
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-19 09:29:14 +0100

    Added vuetify to main

--- frontend/summit-finder/src/main.js
+++ frontend/summit-finder/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from 'vuetify'
+import 'Vuetify/dist/vuetify.min.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app
+    .use(vuetify)
+    .mount('#app');
