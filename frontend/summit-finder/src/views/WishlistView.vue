496033f0799364e815a6187d844e7452e1ed301b
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2024-02-20 18:55:34 +0100

    Added wishlist control for users and admins

--- frontend/summit-finder/src/views/WishlistView.vue
+++ frontend/summit-finder/src/views/WishlistView.vue
@@ -8,7 +8,10 @@ export default {
   name: "WishlistView",
   data() {
     return {
-    wishlist: []
+      wishlist: [],
+      boardIsVisible: false,
+      loggedIn: localStorage.getItem('user'),
+      username: localStorage.getItem('username')
     }
   },
   methods: {
@@ -19,7 +22,7 @@ export default {
             console.log("Wishlist retrieved successfully");
             this.wishlist = response.data.data.wishlist;
           }).catch((error) => {
-            this.wishlist = null;
+            this.wishlist = [];
             console.log(`Error while retrieving the wishlist: ${error}`);
           });
     },
@@ -33,6 +36,42 @@ export default {
           }).catch((error) => {
             console.log(`Error while deleting the wish: ${error}`);
           })
+    },
+    async getAllWishlists() {
+      await axios
+          .get(`${WISHLIST_URL}/all`, { headers: authHeader(),})
+          .then((response) => {
+            console.log("All wishlists retrieved successfully");
+            window.alert("All wishlists retrieved successfully");
+            this.wishlist = response.data.data.wishlists;
+          }).catch((error) => {
+            console.log(`Error while retrieving all wishlists: ${error}`);
+          })
+    },
+    async deleteAllWishlists() {
+      await axios
+          .delete(`${WISHLIST_URL}/all`, { headers: authHeader(),})
+          .then(() => {
+            console.log("All wishlists deleted successfully");
+            window.alert("All wishlists deleted successfully");
+            this.$router.go({ path:'/wishlist'});
+          }).catch((error) => {
+            console.log(`Error while deleting all wishlists: ${error}`);
+          })
+    },
+    async deleteWishlist() {
+      await axios
+          .delete(WISHLIST_URL, { headers: authHeader(),})
+          .then(() => {
+            console.log("Wishlists deleted successfully");
+            window.alert("Wishlists deleted successfully");
+            this.$router.go({ path:'/wishlist'});
+          }).catch((error) => {
+            console.log(`Error while deleting the wishlists: ${error}`);
+          })
+    },
+    toggleBoard() {
+      this.boardIsVisible = !this.boardIsVisible;
     }
   },
   mounted() {
@@ -42,21 +81,103 @@ export default {
 </script>
 
 <template>
+  <div class="w-100 text-center" v-if="wishlist.length > -1 && loggedIn" @click="toggleBoard">
+    <v-icon
+        v-if="!boardIsVisible"
+        icon="mdi-chevron-down"
+        size="large"
+    ></v-icon>
+    <v-icon
+        v-if="boardIsVisible"
+        icon="mdi-chevron-up"
+        size="large"
+    ></v-icon>
+
+    <v-expand-transition>
+      <v-card
+          v-if="boardIsVisible"
+          class="ma-auto pa-5"
+          elevation="8"
+          width="500"
+          rounded="lg"
+      >
+        <b>{{ username }} board</b>
+        <v-btn
+            block
+            class="my-5"
+            color="red"
+            size="large"
+            variant="tonal"
+            @click="deleteWishlist"
+        >Delete wishlist</v-btn>
+        <br>
+        <v-divider color="grey"/>
+        <br>
+        <b>Admin Board</b>
+        <v-btn
+            block
+            class="my-5"
+            color="red"
+            size="large"
+            variant="tonal"
+            @click="getAllWishlists"
+        >Get wishlists</v-btn>
+        <v-btn
+            block
+            class="my-5"
+            color="red"
+            size="large"
+            variant="tonal"
+            @click="deleteAllWishlists"
+        >Delete wishlists</v-btn>
+      </v-card>
+    </v-expand-transition>
+  </div>
+
+  <div
+      v-if="wishlist.length === 0 && loggedIn"
+      class="text-center my-10"
+  >
+    <h2>No wishes yet. Search for peaks and huts!</h2>
+  </div>
+
+  <div
+      v-if="!loggedIn"
+      class="text-center my-10"
+  >
+    <h2>You have to sign in to see your wishlist!</h2>
+  </div>
 
   <v-card
-      v-show="wishlist"
+      v-show="wishlist && loggedIn"
       v-for="wish in wishlist" :key="wish"
-      class="ma-auto pa-5"
+      class="ma-auto py-1 px-5"
       elevation="8"
       width="500"
       rounded="lg"
   >
+    <div
+      class="text-center my-2"
+    >
+      <b>From {{ wish.originName }} to {{ wish.destinationName }}</b>
+    </div>
+    <div class="text-center"><v-icon icon="mdi-image-filter-hdr"></v-icon></div>
+    <div
+        class="text-center"
+        v-for="peak in wish.peaks"
+        :key="peak"
+    >
+      {{ peak }} <br>
+    </div>
     <br>
-    From {{ wish.originName }} to {{ wish.destinationName }}
-    <br>
-    Saved peaks: {{ wish.peaks }}
-    <br>
-    Saved huts: {{ wish.huts }}
+    <div class="text-center"><v-icon icon="mdi-home-map-marker"></v-icon></div>
+    <div
+        class="text-center"
+        v-for="hut in wish.huts"
+        :key="hut"
+    >
+      {{ hut }} <br>
+    </div>
     <br>
     <v-btn
         block
@@ -67,7 +188,6 @@ export default {
         @click="deleteWish(wish._id)"
     >Delete</v-btn>
   </v-card>
-
 </template>
 
 <style scoped>
